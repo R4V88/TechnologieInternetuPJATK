16fdfe9765d671cc2d46871c50b2251e61b023df
Rafal <sochacki.rfl@gmail.com>
2019-11-09 20:35:46 +0100

    feat: fightinig with styles...

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -15,36 +15,35 @@ if ('serviceWorker' in navigator) {
 }
 
 
-//Zadanie 1
-function czyTrojkat(a ,b, c) {
+// //Zadanie 1
+// function czyTrojkat(a, b, c) {
 
-  let numArray = [a, b, c];
-  numArray.sort(sortNumber);
+//   let numArray = [a, b, c];
+//   numArray.sort(sortNumber);
 
-  if(Math.sqrt(Math.pow(numArray[0]) + Math.pow(numArray[1])) == Math.sqrt(Math.pow(numArray[2]))) {
-      return console.log(true);
-  } else {
-      return console.log(false);
-  }
-}
+//   if (Math.sqrt(Math.pow(numArray[0]) + Math.pow(numArray[1])) == Math.sqrt(Math.pow(numArray[2]))) {
+//     return console.log(true);
+//   } else {
+//     return console.log(false);
+//   }
+// }
 
-function sortNumber(a, b){
-  return a - b;
-}
+// function sortNumber(a, b) {
+//   return a - b;
+// }
 
-//Zadanie 2
+// //Zadanie 2
 
-function podzielne(a, b, c){
-  const przedzial = new Array(b);
+// function podzielne(a, b, c) {
+//   const przedzial = new Array(b);
 
-  for (let i = a; i <= przedzial.length; a++){
-      przedzial.push[i];
-  }
-  for (const element in object) {
-    if(element % c == 0)  
-    return console.log(element);
-    }
-}
-
-czyTrojkat(3, 4 ,5);
-podzielne(1, 50);
+//   for (let i = a; i <= przedzial.length; a++) {
+//     przedzial.push[i];
+//   }
+//   for (const element in przedzial) {
+//     if (element % c == 0)
+//       return console.log(element);
+//   }
+// }
+// czyTrojkat(3, 4, 5);
+// podzielne(1, 50);
